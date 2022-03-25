6ba106d3ef04471a299b7a361f7fdbfe482250eb
Lee Sanderson <lee.sndrsn@gmail.com>
2022-03-25 23:38:22 +0000

    first implementation of image converter wrapper

---
+++ WkHtmlConverter/WkHtmlConverter/ImageConverterApiWrapper.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace WkHtmlConverter
+{
+    internal sealed class ImageConverterApiWrapper : IDisposable
+    {
+        private bool _disposed;
+
+        public bool IsLoaded { get; private set; } = false;
+
+        public bool ExtendedQt => WkHtmlToXBindings.wkhtmltoimage_extended_qt() == 1;
+
+        public string LibraryVersion => Marshal.PtrToStringAnsi(WkHtmlToXBindings.wkhtmltoimage_version())!;
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                WkHtmlToXBindings.wkhtmltopdf_deinit();
+
+                _disposed = true;
+            }
+        }
+
+        ~ImageConverterApiWrapper()
+        {
+            Dispose(false);
+        }
+
+        public void Load()
+        {
+
+            if (IsLoaded)
+            {
+                return;
+            }
+
+            if (WkHtmlToXBindings.wkhtmltoimage_init(0) == 1)
+            {
+                IsLoaded = true;
+            }
+        }
+
+        public IntPtr CreateGlobalSettings() => 
+            WkHtmlToXBindings.wkhtmltoimage_create_global_settings();
+
+        public int SetGlobalSetting(IntPtr settings, string name, string value) =>
+            WkHtmlToXBindings.wkhtmltoimage_set_global_setting(settings, name, value);
+
+        public unsafe string GetGlobalSetting(IntPtr settings, string name) =>
+            BufferExtensions.InvokeBufferToUtf8Action(buffer =>
+            {
+                fixed (byte* tempBuffer = buffer)
+                {
+                    WkHtmlToXBindings.wkhtmltoimage_get_global_setting(settings, name, tempBuffer, buffer.Length);
+                }
+            });
+
+        public void DestroyGlobalSetting(IntPtr settings) =>
+            WkHtmlToXBindings.wkhtmltoimage_destroy_global_settings(settings);
+
+        public IntPtr CreateConverter(IntPtr globalSettings) =>
+            WkHtmlToXBindings.wkhtmltoimage_create_converter(globalSettings);
+
+
+        public bool Convert(IntPtr converter) => WkHtmlToXBindings.wkhtmltoimage_convert(converter) == 1;
+
+        public void DestroyConverter(IntPtr converter) => WkHtmlToXBindings.wkhtmltoimage_destroy_converter(converter);
+
+        public byte[] GetConversionResult(IntPtr converter) => BufferExtensions.MarshalReturnBuffer(() =>
+        {
+            var dataLength = WkHtmlToXBindings.wkhtmltoimage_get_output(converter, out var dataPointer);
+            return (dataPointer, dataLength);
+        });
+
+        public void SetPhaseChangedCallback(IntPtr converter, VoidCallback callback) =>
+            WkHtmlToXBindings.wkhtmltoimage_set_phase_changed_callback(converter, callback);
+
+        public void SetProgressChangedCallback(IntPtr converter, IntCallback callback) =>
+            WkHtmlToXBindings.wkhtmltoimage_set_progress_changed_callback(converter, callback);
+
+        public void SetFinishedCallback(IntPtr converter, IntCallback callback) =>
+            WkHtmlToXBindings.wkhtmltoimage_set_finished_callback(converter, callback);
+
+        public void SetDebugCallback(IntPtr converter, StringCallback callback) =>
+            WkHtmlToXBindings.wkhtmltoimage_set_debug_callback(converter, callback);
+
+        public void SetInfoCallback(IntPtr converter, StringCallback callback) =>
+            WkHtmlToXBindings.wkhtmltoimage_set_info_callback(converter, callback);
+
+        public void SetWarningCallback(IntPtr converter, StringCallback callback) =>
+            WkHtmlToXBindings.wkhtmltoimage_set_warning_callback(converter, callback);
+
+        public void SetErrorCallback(IntPtr converter, StringCallback callback) =>
+            WkHtmlToXBindings.wkhtmltoimage_set_error_callback(converter, callback);
+
+        public int GetPhaseCount(IntPtr converter) => WkHtmlToXBindings.wkhtmltoimage_phase_count(converter);
+
+        public int GetCurrentPhase(IntPtr converter) => WkHtmlToXBindings.wkhtmltoimage_current_phase(converter);
+
+        public string GetCurrentPhaseDescription(IntPtr converter, int phase) => 
+            Marshal.PtrToStringAnsi(WkHtmlToXBindings.wkhtmltoimage_phase_description(converter, phase)) ?? string.Empty;
+
+        public string GetProgressString(IntPtr converter) =>
+            Marshal.PtrToStringAnsi(WkHtmlToXBindings.wkhtmltoimage_progress_string(converter)) ?? string.Empty;
+
+        public int GetHttpErrorCode(IntPtr converter) => WkHtmlToXBindings.wkhtmltoimage_http_error_code(converter);
+    }
+
+    internal static class BufferExtensions
+    {
+        public static string ToUtf8String(this byte[] buffer, int offset = 0)
+        {
+            var nullTerminatorIndex = Array.FindIndex(buffer, 0, (x) => x == 0);
+            nullTerminatorIndex = (nullTerminatorIndex == -1) ? buffer.Length : nullTerminatorIndex;
+            return Encoding.UTF8.GetString(buffer, 0, nullTerminatorIndex);
+        }
+
+        public static string InvokeBufferToUtf8Action(Action<byte[]> bufferAction, int bufferSize = 2048)
+        {
+            var buffer = new byte[bufferSize];
+            bufferAction(buffer);
+            return buffer.ToUtf8String();
+        }
+
+        public static byte[] MarshalReturnBuffer(Func<(IntPtr, long)> apiFunction)
+        {
+            var (dataPointer, bufferLength) = apiFunction();
+            var buffer = new byte[bufferLength];
+            Marshal.Copy(dataPointer, buffer, 0, (int)bufferLength);
+            return buffer;
+        }
+    }
+}
--- WkHtmlConverter/WkHtmlConverter/WkHtmlToXBindings.cs
+++ WkHtmlConverter/WkHtmlConverter/WkHtmlToXBindings.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 namespace WkHtmlConverter
 {
-    public static unsafe class WkHtmlToXBindings
+    internal static unsafe class WkHtmlToXBindings
     {
         private const string LibWkhtmlToXDllName = "libwkhtmltox";
 
@@ -13,13 +13,8 @@ namespace WkHtmlConverter
 
         // PDF conversion functions 
         // See: https://github.com/wkhtmltopdf/wkhtmltopdf/blob/master/src/lib/pdf.h
+        // And: https://github.com/wkhtmltopdf/wkhtmltopdf/blob/master/src/lib/pdf_c_bindings.cc
         #region HTML to PDF bindings
-        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
-        public static extern int wkhtmltopdf_extended_qt();
-
-        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
-        public static extern IntPtr wkhtmltopdf_version();
-
         [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
         public static extern int wkhtmltopdf_init(int useGraphics);
 
@@ -27,7 +22,22 @@ namespace WkHtmlConverter
         public static extern int wkhtmltopdf_deinit();
 
         [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int wkhtmltopdf_extended_qt();
+
+        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr wkhtmltopdf_version();
+
+        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr wkhtmltopdf_create_global_settings();
+
+        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void wkhtmltopdf_destroy_global_settings(IntPtr settings);
+
+        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr wkhtmltopdf_create_object_settings();
+
+        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void wkhtmltopdf_destroy_object_settings(IntPtr settings);
 
         [DllImport(LibWkhtmlToXDllName, CharSet = CharSet)]
         public static extern int wkhtmltopdf_set_global_setting(IntPtr settings,
@@ -43,12 +53,6 @@ namespace WkHtmlConverter
             string name,
             byte* value, int valueSize);
 
-        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
-        public static extern int wkhtmltopdf_destroy_global_settings(IntPtr settings);
-
-        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
-        public static extern IntPtr wkhtmltopdf_create_object_settings();
-
         [DllImport(LibWkhtmlToXDllName, CharSet = CharSet)] 
         public static extern int wkhtmltopdf_set_object_setting(IntPtr settings,
             [MarshalAs(Lputf8Str)]
@@ -63,15 +67,35 @@ namespace WkHtmlConverter
             byte* value, int valueSize);
 
         [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
-        public static extern int wkhtmltopdf_destroy_object_settings(IntPtr settings);
-
-        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr wkhtmltopdf_create_converter(IntPtr globalSettings);
 
         [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void wkhtmltopdf_add_object(IntPtr converter,
-            IntPtr objectSettings,
-            byte[] data);
+        public static extern void wkhtmltopdf_destroy_converter(IntPtr converter);
+
+        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void wkhtmltopdf_set_debug_callback(IntPtr converter, [MarshalAs(UnmanagedType.FunctionPtr)] StringCallback callback);
+
+        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void wkhtmltopdf_set_info_callback(IntPtr converter, [MarshalAs(UnmanagedType.FunctionPtr)] StringCallback callback);
+
+        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void wkhtmltopdf_set_warning_callback(IntPtr converter, [MarshalAs(UnmanagedType.FunctionPtr)] StringCallback callback);
+
+        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void wkhtmltopdf_set_error_callback(IntPtr converter, [MarshalAs(UnmanagedType.FunctionPtr)] StringCallback callback);
+
+        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void wkhtmltopdf_set_phase_changed_callback(IntPtr converter, [MarshalAs(UnmanagedType.FunctionPtr)] VoidCallback callback);
+
+        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void wkhtmltopdf_set_progress_changed_callback(IntPtr converter, [MarshalAs(UnmanagedType.FunctionPtr)] VoidCallback callback);
+
+        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void wkhtmltopdf_set_finished_callback(IntPtr converter, [MarshalAs(UnmanagedType.FunctionPtr)] IntCallback callback);
+
+
+        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int wkhtmltopdf_convert(IntPtr converter);
 
         [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
         public static extern void wkhtmltopdf_add_object(IntPtr converter,
@@ -79,34 +103,10 @@ namespace WkHtmlConverter
             [MarshalAs(Lputf8Str)] string data);
 
         [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
-        public static extern bool wkhtmltopdf_convert(IntPtr converter);
-
-        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void wkhtmltopdf_destroy_converter(IntPtr converter);
-
-        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
-        public static extern int wkhtmltopdf_get_output(IntPtr converter, out IntPtr data);
-
-        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
-        public static extern int wkhtmltopdf_set_phase_changed_callback(IntPtr converter, [MarshalAs(UnmanagedType.FunctionPtr)] VoidCallback callback);
-
-        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
-        public static extern int wkhtmltopdf_set_progress_changed_callback(IntPtr converter, [MarshalAs(UnmanagedType.FunctionPtr)] VoidCallback callback);
-
-        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
-        public static extern int wkhtmltopdf_set_finished_callback(IntPtr converter, [MarshalAs(UnmanagedType.FunctionPtr)] IntCallback callback);
-
-        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
-        public static extern int wkhtmltopdf_set_warning_callback(IntPtr converter, [MarshalAs(UnmanagedType.FunctionPtr)] StringCallback callback);
-
-        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
-        public static extern int wkhtmltopdf_set_error_callback(IntPtr converter, [MarshalAs(UnmanagedType.FunctionPtr)] StringCallback callback);
+        public static extern int wkhtmltopdf_current_phase(IntPtr converter);
 
         [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
         public static extern int wkhtmltopdf_phase_count(IntPtr converter);
-
-        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
-        public static extern int wkhtmltopdf_current_phase(IntPtr converter);
 
         [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr wkhtmltopdf_phase_description(IntPtr converter, int phase);
@@ -116,11 +116,15 @@ namespace WkHtmlConverter
 
         [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
         public static extern int wkhtmltopdf_http_error_code(IntPtr converter);
-
+        
+        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
+        public static extern long wkhtmltopdf_get_output(IntPtr converter, out IntPtr data);
+        
         #endregion
 
         // Image conversion functions 
         // See: https://github.com/wkhtmltopdf/wkhtmltopdf/blob/master/src/lib/image.h
+        // And: https://github.com/wkhtmltopdf/wkhtmltopdf/blob/master/src/lib/image_c_bindings.cc
         #region HTML to Image bindings
         [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
         public static extern int wkhtmltoimage_init(int useGraphics);
@@ -136,6 +140,10 @@ namespace WkHtmlConverter
 
         [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr wkhtmltoimage_create_global_settings();
+
+        [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int wkhtmltoimage_destroy_global_settings(IntPtr settings);
+
 
         [DllImport(LibWkhtmlToXDllName, CharSet = CharSet)]
         public static extern int wkhtmltoimage_set_global_setting(IntPtr settings,
@@ -197,17 +205,17 @@ namespace WkHtmlConverter
         public static extern int wkhtmltoimage_http_error_code(IntPtr converter);
 
         [DllImport(LibWkhtmlToXDllName, CharSet = CharSet, CallingConvention = CallingConvention.Cdecl)]
-        public static extern int wkhtmltoimage_get_output(IntPtr converter, out IntPtr data);
+        public static extern long wkhtmltoimage_get_output(IntPtr converter, out IntPtr data);
 
         #endregion
     }
 
     [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    public delegate void StringCallback(IntPtr converter, [MarshalAs(UnmanagedType.LPStr)] string str);
+    internal delegate void StringCallback(IntPtr converter, [MarshalAs(UnmanagedType.LPStr)] string str);
 
     [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    public delegate void IntCallback(IntPtr converter, int integer);
+    internal delegate void IntCallback(IntPtr converter, int integer);
 
     [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    public delegate void VoidCallback(IntPtr converter);
+    internal delegate void VoidCallback(IntPtr converter);
 }
