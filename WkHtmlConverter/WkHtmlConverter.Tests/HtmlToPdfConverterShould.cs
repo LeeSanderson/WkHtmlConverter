86fe9bb8b0158b62667b5c7adcd7757508130df2
Lee Sanderson <lee.sndrsn@gmail.com>
2022-04-27 13:15:16 +0100

    Added test for PDF conversions

---
+++ WkHtmlConverter/WkHtmlConverter.Tests/HtmlToPdfConverterShould.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using FluentAssertions;
+using VerifyTests;
+using VerifyXunit;
+using Xunit;
+
+namespace WkHtmlConverter.Tests
+{
+    public class HtmlToPdfConverterShould : VerifyBase
+    {
+        public HtmlToPdfConverterShould() : base()
+        {
+        }
+
+        static HtmlToPdfConverterShould()
+        {
+            VerifyDocNet.Initialize();
+            VerifyImageMagick.RegisterComparers(threshold: 0.13, ImageMagick.ErrorMetric.PerceptualHash);
+        }
+
+        [Fact]
+        public async Task CreateExpectedPdfFromHtml()
+        {
+            const string generatedImageFileName = $"{nameof(CreateExpectedPdfFromHtml)}.pdf";
+            var converter = new HtmlToPdfConverter();
+            var pdfConversionSettings = new PdfConversionSettings
+            {
+                GlobalSettings = new PdfConversionGlobalSettings(),
+                Sections = new List<PdfConversionObjectSettings>
+                {
+                    new()
+                }
+            };
+
+            var result = await converter.ConvertAsync(pdfConversionSettings, "<h1>Hello World</h1>");
+
+            result.Length.Should().BeGreaterThan(0);
+            await File.WriteAllBytesAsync(generatedImageFileName, result);
+            await VerifyFile(generatedImageFileName);
+        }
+
+    }
+}
